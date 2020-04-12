fc8df8f2018300f892b63a108dbeed92bd6d040a
Frank Kany <frankkany@gmail.com>
2020-04-12 18:55:21 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-kany"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ze6cxqlfli.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_98yFHactN",
+    APP_CLIENT_ID: "1t518c3ke914oqeguobgpkkt85",
+    IDENTITY_POOL_ID: "us-east-1:6c0d11af-9430-4df2-85b1-abda877bb13c"
+  }
+};
